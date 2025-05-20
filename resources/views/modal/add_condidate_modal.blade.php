2dcca6d2261796f5bddacf034d4717497dd27bed
Syed Ammad Hussain Shah <syedammad900@gmail.com>
2025-05-20 18:57:55 +0500

    changes

--- resources/views/modal/add_condidate_modal.blade.php
+++ resources/views/modal/add_condidate_modal.blade.php
@@ -388,7 +388,7 @@
         });
 
         $(document).ready(function() {
-                $('.js-example-basic-multiple').select2();
-            });
+            $('.js-example-basic-multiple').select2();
+        });
     </script>
 @endsection
